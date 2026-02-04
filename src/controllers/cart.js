2788adc5e7b5312f7381d4db6602ede719c57e67
Ahmed Alsalim <ahmed123t@gmail.com>
2026-02-05 01:15:08 +0300

    improve sql queries for images

--- src/controllers/cart.js
+++ src/controllers/cart.js
@@ -3,8 +3,17 @@ const sql = require('../config/db');
 async function getCart(req, res) {
   const accountId = req.user.id;
   try {
-    const cart =
-      await sql`SELECT * FROM cart_items WHERE account_id = ${accountId}`;
+    const cart = await sql`
+        SELECT
+          c.id as cart_item_id, c.quantity,
+          p.code, p.discount, p.name, p.price, p.quantity as stock, p.variant_code, p.description, p.id as product_id, 
+          pi.url as image_url
+        FROM
+          cart_items c
+          JOIN products p ON c.product_id = p.id
+          LEFT JOIN lateral (select url FROM product_images WHERE product_id = p.id limit 1) pi ON true
+        WHERE c.account_id = ${accountId};
+      `;
     res.status(200).json(cart || []);
   } catch (error) {
     res.status(500).send(error.message);
@@ -13,15 +22,15 @@ async function getCart(req, res) {
 
 async function addOrUpdateItem(req, res) {
   const accountId = req.user.id;
-  const { productId, quantity } = req.body || {};
+  const { product_id, quantity } = req.body || {};
 
-  if (!productId || typeof quantity !== 'number') {
+  if (!product_id || typeof quantity !== 'number') {
     return res.status(400).send('Product ID and valid quantity are required');
   }
 
   if (quantity === 0) {
     try {
-      await sql`DELETE FROM cart_items WHERE account_id = ${accountId} AND product_id = ${productId}`;
+      await sql`DELETE FROM cart_items WHERE account_id = ${accountId} AND product_id = ${product_id}`;
 
       return res.sendStatus(200);
     } catch (error) {
@@ -30,7 +39,7 @@ async function addOrUpdateItem(req, res) {
   }
 
   try {
-    await sql`INSERT INTO cart_items (account_id, product_id, quantity) VALUES (${accountId}, ${productId}, ${quantity}) ON CONFLICT (account_id, product_id) DO UPDATE SET quantity = ${quantity}`;
+    await sql`INSERT INTO cart_items (account_id, product_id, quantity) VALUES (${accountId}, ${product_id}, ${quantity}) ON CONFLICT (account_id, product_id) DO UPDATE SET quantity = ${quantity}`;
 
     return res.sendStatus(200);
   } catch (error) {
--- src/controllers/products.js
+++ src/controllers/products.js
@@ -8,15 +8,13 @@ async function createProduct({ body }, res) {
     const result = await sql`INSERT INTO products (
         category, code,
         description, discount,
-        image_url, variant_code,
-        name, price,
-        quantity
+        variant_code, name,
+        price, quantity
       ) VALUES (
         ${body.category || null}, ${body.code},
         ${body.description || null}, ${body.discount || null},
-        ${body.image_url || null}, ${body.variant_code || null},
-        ${body.name || null}, ${body.price || null},
-        ${body.quantity || null}
+        ${body.variant_code || null}, ${body.name || null},
+        ${body.price || null}, ${body.quantity || null}
       )
         RETURNING id`;
 
@@ -39,7 +37,7 @@ async function updateProduct(req, res) {
   try {
     const result = await sql`UPDATE products SET ${sql(
       req.body,
-      Object.keys(req.body)
+      Object.keys(req.body),
     )}
       WHERE id = ${id}
       RETURNING id`;
@@ -57,8 +55,13 @@ async function listProducts(req, res) {
   const limit = parseInt(req.query.limit, 10) || 10;
   const offset = parseInt(req.query.offset, 10) || 0;
   try {
-    const products =
-      await sql`SELECT * FROM products LIMIT ${limit} OFFSET ${offset}`;
+    const products = await sql`
+      select p.*, array_agg(pi.url) as image_url
+      from products p
+      join product_images pi on p.id = pi.product_id
+      group by p.id
+      LIMIT ${limit} OFFSET ${offset}
+    `;
     res.status(200).json(products);
   } catch (error) {
     res.status(500).send(error.message);
@@ -71,7 +74,13 @@ async function getProductById(req, res) {
     return res.status(400).send('Product ID is required');
   }
   try {
-    const product = await sql`SELECT * FROM products WHERE id = ${id}`;
+    const product = await sql`
+      select p.*, array_agg(pi.url) as image_url
+      from products p
+      join product_images pi on p.id = pi.product_id
+      WHERE p.id = ${id}
+      group by p.id
+    `;
     if (product.length === 0) {
       return res.status(404).send('Product not found');
     }
