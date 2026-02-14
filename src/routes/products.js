fa6076b727ea217fa71f729496875088de903a72
Ahmed Alsalim <ahmed123t@gmail.com>
2026-02-15 02:11:12 +0300

    re-order

--- src/routes/products.js
+++ src/routes/products.js
@@ -1,19 +1,25 @@
 const { Router } = require('express');
 const { requireAuth, requireAdmin } = require('../middlewares/auth');
 const {
+  addProductImage,
   createProduct,
-  updateProduct,
-  listProducts,
-  getProductById,
   deleteProduct,
+  deleteProductImage,
+  getProductById,
+  getProductImages,
+  listProducts,
+  updateProduct,
 } = require('../controllers/products');
 
 const router = Router();
 
+router.get('/:id', getProductById);
+router.get('/:id/images', getProductImages);
+router.get('/list', listProducts);
+router.post('/:id/images', requireAuth, requireAdmin, addProductImage);
 router.post('/', requireAuth, requireAdmin, createProduct);
 router.put('/:id', requireAuth, requireAdmin, updateProduct);
-router.get('/list', listProducts);
-router.get('/:id', getProductById);
 router.delete('/:id', requireAuth, requireAdmin, deleteProduct);
+router.delete('/:id/images', requireAuth, requireAdmin, deleteProductImage);
 
 module.exports = router;
